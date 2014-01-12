33b4791672b24994c4a8ab0854f9b281bd9d5021
Sebastian Bauer <admin@e-back.net>
2014-01-12 02:14:51 +0100

    * add Cash On Delivery backend  * add Regular transfer backend

---
+++ getpaid/backends/cod/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.importlib import import_module
+from django.utils.translation import ugettext_lazy as _
+from getpaid.backends import PaymentProcessorBase
+
+
+class PaymentProcessor(PaymentProcessorBase):
+    BACKEND = 'getpaid.backends.cod'
+    BACKEND_NAME = _(u'Cash on delivery')
+    BACKEND_ACCEPTED_CURRENCY = ('PLN', 'EUR', 'USD')
+
+    def get_gateway_url(self, request):
+        """
+        Routes a payment to view from configurations.
+        """
+
+        module_name = PaymentProcessor.get_backend_setting('module_name')
+        module = import_module(module_name)
+        url = module.get_cod_url(self.payment)
+        if not isinstance(url, basestring):
+            raise ImproperlyConfigured('COD backend have wrong url class in config')
+        return url, 'GET', {}
---
+++ getpaid/backends/regular_transfer/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+from django.core.exceptions import ImproperlyConfigured
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.utils.importlib import import_module
+from django.utils.translation import ugettext_lazy as _
+from getpaid.backends import PaymentProcessorBase
+
+
+class PaymentProcessor(PaymentProcessorBase):
+    BACKEND = 'getpaid.backends.regular_transfer'
+    BACKEND_NAME = _(u'Regular transfer')
+    BACKEND_ACCEPTED_CURRENCY = ('PLN', 'EUR', 'USD')
+
+    def get_gateway_url(self, request):
+        """
+        Routes a payment to view from configurations.
+        """
+
+        module_name = PaymentProcessor.get_backend_setting('module_name')
+        module = import_module(module_name)
+        url = module.get_regular_transfer_url(self.payment)
+        if not isinstance(url, basestring):
+            raise ImproperlyConfigured('Regular transfer backend have wrong url class in config')
+        return url, 'GET', {}
