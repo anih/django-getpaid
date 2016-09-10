da6e4a4541e66e503f813a3291a2b312aeb0231b
Sebastian Bauer <admin@e-back.net>
2016-09-10 14:32:04 +0200

    first try of migrate paypal backend to django-paypal

--- getpaid/backends/paypal/__init__.py
+++ getpaid/backends/paypal/__init__.py
@@ -1,11 +1,16 @@
 import datetime
 import logging
-import urllib
 from decimal import Decimal
 
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.utils.timezone import utc
 from django.utils.translation import ugettext_lazy as _
+from getpaid.models import Payment
+from paypal.standard.conf import SANDBOX_POSTBACK_ENDPOINT, POSTBACK_ENDPOINT
+from paypal.standard.forms import PayPalPaymentsForm
+from paypal.standard.models import ST_PP_COMPLETED, ST_PP_DENIED, ST_PP_REFUSED, ST_PP_CANCELLED
+from paypal.standard.ipn.signals import valid_ipn_received
 
 from getpaid import signals
 from getpaid.backends import PaymentProcessorBase
@@ -14,71 +19,42 @@ from getpaid.utils import get_domain
 logger = logging.getLogger('getpaid.backends.paypal')
 
 
-class paypalTransactionStatus:
-    ST_PP_ACTIVE = 'Active'
-    ST_PP_CANCELLED = 'Cancelled'
-    ST_PP_CLEARED = 'Cleared'
-    ST_PP_COMPLETED = 'Completed'
-    ST_PP_DENIED = 'Denied'
-    ST_PP_PAID = 'Paid'
-    ST_PP_PENDING = 'Pending'
-    ST_PP_PROCESSED = 'Processed'
-    ST_PP_REFUSED = 'Refused'
-    ST_PP_REVERSED = 'Reversed'
-    ST_PP_REWARDED = 'Rewarded'
-    ST_PP_UNCLAIMED = 'Unclaimed'
-    ST_PP_UNCLEARED = 'Uncleared'
-
-
 class PaymentProcessor(PaymentProcessorBase):
     BACKEND = 'getpaid.backends.paypal'
     BACKEND_NAME = _('paypal')
     BACKEND_ACCEPTED_CURRENCY = ('PLN', 'USD', 'EUR', 'GPB')
     BACKEND_LOGO_URL = 'getpaid/backends/paypal/paypal_logo.png'
-    # API Endpoints.
-    POSTBACK_ENDPOINT = "https://www.paypal.com/cgi-bin/webscr"
-    SANDBOX_POSTBACK_ENDPOINT = "https://www.sandbox.paypal.com/cgi-bin/webscr"
 
     @staticmethod
-    def online(ipn_obj, flag, form, secure):
-
-        if flag is not None:
-            ipn_obj.set_flag(flag)
-        else:
-            # Secrets should only be used over SSL.
-            if secure:
-                ipn_obj.verify_secret(form, secure)
-            else:
-                business = PaymentProcessor.get_backend_setting('business') if not PaymentProcessor.get_backend_setting(
-                    'test') \
-                    else PaymentProcessor.get_backend_setting('test_business')
-                ipn_obj.verify(receiver_email=business)
-
-        # if verification ended in error, return it:
-        if ipn_obj.flag:
-            return ipn_obj.flag_info
-
-        payment = ipn_obj.payment
+    def ipn_signal_handler(sender, **kwargs):
+        ipn_obj = sender
+        payment = Payment.objects.get(pk=ipn_obj.custom)
         payment.external_id = ipn_obj.txn_id
         payment.description = ipn_obj.item_name
 
-        if ipn_obj.payment_status == paypalTransactionStatus.ST_PP_COMPLETED:
+        if ipn_obj.payment_status == ST_PP_COMPLETED:
+            business = PaymentProcessor.get_business()
+            if ipn_obj.receiver_email != business:
+                # Not a valid payment
+                return
+
             payment.amount_paid = Decimal(ipn_obj.mc_gross)
             payment.paid_on = datetime.datetime.utcnow().replace(tzinfo=utc)
+            payment.save()
             if Decimal(ipn_obj.mc_gross) >= payment.amount:
                 logger.debug('paypal: status PAID')
                 payment.change_status('paid')
             else:
                 payment.change_status('partially_paid')
-        elif ipn_obj.payment_status in (paypalTransactionStatus.ST_PP_CANCELLED,
-                                        paypalTransactionStatus.ST_PP_DENIED,
-                                        paypalTransactionStatus.ST_PP_REFUSED):
+
+        elif ipn_obj.payment_status in (ST_PP_CANCELLED,
+                                        ST_PP_DENIED,
+                                        ST_PP_REFUSED):
             logger.debug('paypal: status FAILED')
+            payment.save()
             payment.change_status('failed')
         else:
             logger.error('paypal: unknown status %d' % ipn_obj.payment_status)
-
-        return 'OK'
 
     def get_return_url(self, type, pk=None):
         kwargs = {'pk': pk} if pk else {}
@@ -89,10 +65,19 @@ class PaymentProcessor(PaymentProcessorBase):
         else:
             return 'http://%s%s' % (domain, url)
 
+    @staticmethod
+    def is_test_mode():
+        return getattr(settings, 'PAYPAL_TEST', True)
+
     @property
-    def _get_gateway_url(cls):
-        test = PaymentProcessor.get_backend_setting('test', True)
-        return cls.SANDBOX_POSTBACK_ENDPOINT if test else cls.POSTBACK_ENDPOINT
+    def _get_gateway_url(self):
+        return SANDBOX_POSTBACK_ENDPOINT if self.is_test_mode() else POSTBACK_ENDPOINT
+
+    @staticmethod
+    def get_business():
+        if PaymentProcessor.is_test_mode():
+            return PaymentProcessor.get_backend_setting('test_business')
+        return PaymentProcessor.get_backend_setting('business')
 
     def get_gateway_url(self, request):
         """
@@ -108,28 +93,43 @@ class PaymentProcessor(PaymentProcessorBase):
         }
 
         signals.user_data_query.send(sender=None, order=self.payment.order, user_data=user_data)
-        business = PaymentProcessor.get_backend_setting('business') if not PaymentProcessor.get_backend_setting(
-            'test') \
-            else PaymentProcessor.get_backend_setting('test_business')
-        params = {
-            'business': business,
-            'cmd': "_xclick",
-            'charset': "UTF-8",
-            'custom': self.payment.pk,
-            'amount': self.payment.amount,
+        paypal_dict = {
+            "business": self.get_business(),
+            "amount": self.payment.amount,
             'currency_code': self.payment.currency.upper(),
             'item_name': self.get_order_description(self.payment, self.payment.order),
-            'item_number': None,
-            'payment_type': 'WLT',
-            'quantity': 1,
-            'return': self.get_return_url('success', self.payment.pk),
+            # "invoice": "unique-invoice-id",
+            "notify_url": self.get_return_url('online'),
+            "return_url": self.get_return_url('success', self.payment.pk),
+            "return": self.get_return_url('success', self.payment.pk),
             'cancel_return': self.get_return_url('failure', self.payment.pk),
-            'notify_url': self.get_return_url('online')
+            'custom': self.payment.pk,
         }
+        return self._get_gateway_url, 'POST', paypal_dict
 
-        # params['logo_url'] = PaymentProcessor.get_backend_setting('logo_url);
-        logger.debug('sending payment to paypal: %s' % str(params))
-        # for key in params.keys():
-        #     params[key] = unicode(params[key]).encode('utf-8')
-        return self._get_gateway_url + '?' + urllib.urlencode(params), 'GET', {}
-        # return self._get_gateway_url, 'POST', params
+    def get_form(self, post_data):
+        return PayPalPaymentsForm(initial=post_data)
+
+
+
+#
+# def show_me_the_money(sender, **kwargs):
+#     ipn_obj = sender
+#     if ipn_obj.payment_status == ST_PP_COMPLETED:
+#         # WARNING !
+#         # Check that the receiver email is the same we previously
+#         # set on the business field request. (The user could tamper
+#         # with those fields on payment form before send it to PayPal)
+#         if ipn_obj.receiver_email != "receiver_email@example.com":
+#             # Not a valid payment
+#             return
+#
+#         # ALSO: for the same reason, you need to check the amount
+#         # received etc. are all what you expect.
+#
+#         # Undertake some action depending upon `ipn_obj`.
+#         if ipn_obj.custom == "Upgrade all users!":
+#             Users.objects.update(paid=True)
+#     else:
+
+valid_ipn_received.connect(PaymentProcessor.ipn_signal_handler)
