54dd94ef98c7454d2166b2cb1a25132017839402
Sebastian Bauer <admin@e-back.net>
2016-09-10 15:20:46 +0200

    first try of migrate paypal backend to django-paypal

---
+++ getpaid/backends/paypal/apps.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+from django.apps import AppConfig
+
+
+class GetPaidPaypalAppConfig(AppConfig):
+    label = 'getpaid_paypal'
+    verbose_name = "GetPaid PayPal backend"
