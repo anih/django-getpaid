88b70480b5094dd4f9e15b3d9d8bdee38b233672
Sebastian Bauer <admin@e-back.net>
2018-01-05 11:14:57 +0100

    dj 1.11

--- getpaid/forms.py
+++ getpaid/forms.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 from django.forms import forms
 from django.forms.fields import ChoiceField, CharField
 from django.forms.models import ModelChoiceField
-from django.forms.widgets import HiddenInput, RadioSelect, RadioFieldRenderer, RadioChoiceInput
+from django.forms.widgets import HiddenInput, RadioSelect
 
 from django.utils.encoding import force_text
 
@@ -12,32 +12,33 @@ from django.utils.translation import ugettext as _
 from getpaid.models import Order
 from .utils import get_backend_choices, import_name
 
+#
+# class PaymentRadioInput(RadioChoiceInput):
+#     def __init__(self, name, value, attrs, choice, index):
+#         super(PaymentRadioInput, self).__init__(name, value, attrs, choice, index)
+#         logo_url = import_name(choice[0]).PaymentProcessor.get_logo_url()
+#         if logo_url:
+#             self.choice_label = mark_safe('<img src="%s%s" alt="%s">' % (
+#                 getattr(settings, 'STATIC_URL', ''),
+#                 logo_url,
+#                 force_text(choice[1]),
+#                 )
+#             )
 
-class PaymentRadioInput(RadioChoiceInput):
-    def __init__(self, name, value, attrs, choice, index):
-        super(PaymentRadioInput, self).__init__(name, value, attrs, choice, index)
-        logo_url = import_name(choice[0]).PaymentProcessor.get_logo_url()
-        if logo_url:
-            self.choice_label = mark_safe('<img src="%s%s" alt="%s">' % (
-                getattr(settings, 'STATIC_URL', ''),
-                logo_url,
-                force_text(choice[1]),
-                )
-            )
-
-
-class PaymentRadioFieldRenderer(RadioFieldRenderer):
-    def __iter__(self):
-        for i, choice in enumerate(self.choices):
-            yield PaymentRadioInput(self.name, self.value, self.attrs.copy(), choice, i)
-
-    def __getitem__(self, idx):
-        choice = self.choices[idx]  # Let the IndexError propogate
-        return PaymentRadioInput(self.name, self.value, self.attrs.copy(), choice, idx)
+#
+# class PaymentRadioFieldRenderer(RadioFieldRenderer):
+#     def __iter__(self):
+#         for i, choice in enumerate(self.choices):
+#             yield PaymentRadioInput(self.name, self.value, self.attrs.copy(), choice, i)
+#
+#     def __getitem__(self, idx):
+#         choice = self.choices[idx]  # Let the IndexError propogate
+#         return PaymentRadioInput(self.name, self.value, self.attrs.copy(), choice, idx)
 
 
 class PaymentRadioSelect(RadioSelect):
-    renderer = PaymentRadioFieldRenderer
+    pass
+    # renderer = PaymentRadioFieldRenderer
 
 
 class PaymentMethodForm(forms.Form):
